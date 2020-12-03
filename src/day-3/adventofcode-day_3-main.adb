8bd5ef55e320a58c4ada8baf2042d0e35f49a8db
persan <per.s.sandberg@bahnhof.se>
2020-12-03 08:14:10 +0100

    Prepare for day 4

--- src/day-3/adventofcode-day_3-main.adb
+++ src/day-3/adventofcode-day_3-main.adb
@@ -5,12 +5,6 @@ procedure Adventofcode.Day_3.Main is
 begin
    Slope.Read ("src/day-3/input");
    Result := Slope.Race (1, 1) * Slope.Race (3, 1) * Slope.Race (5, 1) * Slope.Race (7, 1)  * Slope.Race (1, 2);
-
-   Put_Line (Slope.Race (1, 1)'Img);
    Put_Line (Slope.Race (3, 1)'Img);
-   Put_Line (Slope.Race (5, 1)'Img);
-   Put_Line (Slope.Race (7, 1)'Img);
-   Put_Line (Slope.Race (1, 2)'Img);
-
    Put_Line (Result'Img);
 end Adventofcode.Day_3.Main;
