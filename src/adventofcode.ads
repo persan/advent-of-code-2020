f621dbf72d50bce1e68c626cde9d0fe934bf71f7
persan <per.s.sandberg@bahnhof.se>
2020-12-06 18:26:51 +0100

    Maintanace

--- src/adventofcode.ads
+++ src/adventofcode.ads
@@ -1,3 +1,5 @@
+--  https://adventofcode.com/2020
+
 package Adventofcode is
    pragma Elaborate_Body;
 
--- src/day-3/adventofcode-day_3-main.adb
+++ src/day-3/adventofcode-day_3-main.adb
@@ -4,7 +4,8 @@ procedure Adventofcode.Day_3.Main is
    Result : Long_Long_Integer;
 begin
    Slope.Read ("src/day-3/input");
-   Result := Slope.Race (1, 1) * Slope.Race (3, 1) * Slope.Race (5, 1) * Slope.Race (7, 1)  * Slope.Race (1, 2);
    Put_Line (Slope.Race (3, 1)'Img);
+
+   Result := Slope.Race (1, 1) * Slope.Race (3, 1) * Slope.Race (5, 1) * Slope.Race (7, 1)  * Slope.Race (1, 2);
    Put_Line (Result'Img);
 end Adventofcode.Day_3.Main;
